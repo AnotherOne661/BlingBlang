c284802cd01aa014975267468a725f6e9040afec
Another <159118983+AnotherOne661@users.noreply.github.com>
2024-04-08 10:22:43 +0200

    Update Main.java

--- src/main/java/org/anotherone661/blingblang/Main.java
+++ src/main/java/org/anotherone661/blingblang/Main.java
@@ -5,6 +5,8 @@ import org.anotherone661.blingblang.ui.Launcher;
 public class Main {
     public static void main(String[] args) {
         Launcher launcher = new Launcher();
-        launcher.run();
+        while(true){
+            launcher.run();
+        }
     }
 }
